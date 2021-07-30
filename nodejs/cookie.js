4c57967aa7f0b600b369a82c17c84e6e6d891e00
YJ <lyjoo7413@naver.com>
2021-07-30 20:44:23 +0900

    secure & httpOnly

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -13,6 +13,8 @@ http
         "yummy_cookie=choco",
         "tasty_cookie=strawberry",
         `Permanent=cookies; Max-Age=${60 * 60 * 24 * 30}`,
+        "Secure=Secure; Secure", // 해설>> 일반적인) 쿠키의 이름=값; [진짜 중요한 부분!!]
+        "HttpOnly=HttpOnly; HttpOnly",
       ],
     });
     response.end("Cookie!!");
